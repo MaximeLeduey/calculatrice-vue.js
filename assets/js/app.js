e001ed4a8fd74561797a596526347e2ca5391133
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2022-05-02 11:52:31 +0200

    fonctions

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,15 +5,23 @@ let vueCalc = new Vue({
 
     data: {
         expression: 0,
+        varTampon: '',
     },
 
     methods: {
         appendToNumber: function(n) {
-            if(this.expression == 0){
+            if(this.expression === 0){
                 this.expression = n;
             }
             else {
                 this.expression = "" + this.expression + n;
+                this.varTampon = "" + n;
+            }
+        },
+        appendOperator: function(o) {
+            if(this.varTampon !== o) {
+                this.expression = "" + this.expression + o;
+                this.varTampon = o;
             }
         },
         clear: function() {
@@ -21,6 +29,12 @@ let vueCalc = new Vue({
         },
         percentage: function() {
             this.expression = this.expression / 100;
+        },
+        equal: function() {
+            if(this.varTampon == "." || this.varTampon == "+" || this.varTampon == "-" || this.varTampon == "*" || this.varTampon == "/") {
+                let calcule = parsInt(this.expression);
+                this.expression = calcule;
+            }
         }
     }
 })
